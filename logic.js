9c72d6c26ecb002f156eedf9b022a9f7234889a3
Kevin Brannan <brannake@gmail.com>
2017-10-08 22:03:10 -0700

    Adds appearing text

--- logic.js
+++ logic.js
@@ -1,4 +1,26 @@
 $(document).ready(function(){
 
-	 setTimeout(function(){Materialize.toast("Scroll down to see some of the projects I've worked on!", 3000)}, 2000); 
+	//Handles the quote text appearing on scroll down
+	$(window).on('scroll', function() {
+
+		let element_position = $('.tlt').offset().top;
+		let screen_height = $(window).height();
+		let activation_offset = 0.5;//determines how far up the the page the element needs to be before triggering the function
+		let activation_point = element_position - (screen_height * activation_offset);
+		let max_scroll_height = $('body').height() - screen_height - 5;//-5 for a little bit of buffer
+    	let y_scroll_pos = window.pageYOffset;
+    	let element_in_view = y_scroll_pos > activation_point;
+    
+    	if(element_in_view) {
+  			$('.tlt').textillate({
+  				in: {
+    			effect: 'rollIn',
+    			delayScale: 0.5,
+    			delay: 10,
+    			shuffle: true
+				},
+				loop: false,
+			});
+		}
+	});
 });
